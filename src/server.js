16b4c1255ccf6cfeeff9a8845ca725a7fe306df8
Alexandre <global-atorqueti@icatuseguros.com.br>
2026-03-22 02:19:09 -0300

    Cors

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const corsOptions = {
     
     const allowedOrigins = process.env.CORS_ORIGIN 
       ? process.env.CORS_ORIGIN.split(',').map(o => o.trim())
-      : ['*'];
+      : ['http://localhost:3000', 'http://192.168.0.70:3000'];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
