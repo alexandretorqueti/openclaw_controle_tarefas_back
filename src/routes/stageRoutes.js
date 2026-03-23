a45f5d9ec0ccdc1e3d24aafab01355dc0861bed3
alexandretorqueti <alexandre@globaltecnologia.net>
2026-03-22 22:36:41 -0300

    Migração automática

--- src/routes/stageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stageController = require('../controllers/stageController');
-router.get('/', stageController.getAll);
-router.get('/:id', stageController.getById);
-router.post('/', stageController.create);
-router.put('/:id', stageController.update);
-router.delete('/:id', stageController.delete);
-module.exports = router;
--- src/utils/monitorUtils.js
+++
@@ -1,9 +0,0 @@
-// src/utils/monitorUtils.js
-// Re-exporta utilitarios do sistema de monitoramento para compatibilidade
-const { fileExists } = require('./fileUtils');
-const { segundosToMinutos_Segundos, isProcessAlive } = require('./timeUtils');
-module.exports = {
-    fileExists,
-    segundosToMinutos_Segundos,
-    isProcessAlive
-};
